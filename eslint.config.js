e5176e02aadd9de19c4df1556383ccdc0ae2fa08
kurozzxc <74487494+kurozzxc@users.noreply.github.com>
2025-07-23 17:08:25 +0800

    update

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,8 @@ import js from "@eslint/js";
 import globals from "globals";
 import pluginReact from "eslint-plugin-react";
 import pluginSecurity from "eslint-plugin-security";
+import pluginSecurityNode from "eslint-plugin-security-node";
+import pluginNoUnsanitized from "eslint-plugin-no-unsanitized";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
@@ -14,10 +16,14 @@ export default defineConfig([
       js,
       react: pluginReact,
       security: pluginSecurity,
+      "security-node":pluginSecurityNode,
+      "no-unsanitized": pluginNoUnsanitized,
     },
     rules: {
       ...js.configs.recommended.rules,
       ...pluginReact.configs.flat.recommended.rules,
+      ...pluginSecurityNode.configs.recommended.rules,
+      ...pluginNoUnsanitized.configs.recommended.rules,
       "security/detect-eval-with-expression": "error",
     },
   },
